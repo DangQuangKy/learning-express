8d213b1f06a673d323a7f11eca09b08d2a3de177
Dang Quang Ky <quangky250896@gmail.com>
2025-02-12 11:10:27 +0700

    add project

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const path = require('path');
+const { log } = require('console');
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(morgan('combined')) 
+
+// template engine
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}))
+app.set('view engine','hbs')
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
