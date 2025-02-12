90b8884a4edb42be3395026afed9410f0f65fdab
Dang Quang Ky <quangky250896@gmail.com>
2025-02-12 16:20:34 +0700

    Refactor code: create routes, app/controller

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
-const path = require('path');
-const { log } = require('console');
+const path = require('path')
 const app = express()
-const port = 3000
+const port = 3333
+
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(morgan('combined')) 
+app.use(express.urlencoded({
+    extended: true,
+}))
+
+app.use(express.json())
+// app.use(morgan('combined')) 
 
 // template engine
 app.engine('hbs', handlebars.engine({
@@ -17,9 +23,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine','hbs')
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+// Routes init
+route(app)
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
